5b1ababe8d89e7d492db9558d85ef2ccea1dc928
Samuel K. Gutierrez <samuel@lanl.gov>
2018-03-29 13:42:57 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# SDS Distributed Key-Value
---
+++ src/sdskdv.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2018      Los Alamos National Security, LLC
+ *                         All rights reserved.
+ *
+ * This file is part of the sdsdkv project. See the LICENSE file at the
+ * top-level directory of this distribution.
+ */
+
+/**
+ * @file sdsdkv.h
+ */
+
+#ifndef SDSDKV_H_INCLUDED
+#define SDSDKV_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
