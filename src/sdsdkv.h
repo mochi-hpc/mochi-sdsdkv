4488613fc7026e81598ef1c5e1537c705554148d
Samuel K. Gutierrez <samuel@lanl.gov>
2018-04-10 11:26:01 -0600

    Configury and base infrastructure.

--- src/sdsdkv.h
+++ src/sdsdkv.h
@@ -13,10 +13,45 @@
 #ifndef SDSDKV_H_INCLUDED
 #define SDSDKV_H_INCLUDED
 
+// For exported MPI types.
+#include "mpi.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/** Convenience definition (in case anyone needs this). */
+#define SDSDKV 1
+
+/** Opaque sdsdkv context. */
+struct sdsdkv_t;
+/** Convenience typedef. */
+typedef struct sdsdkv_t sdsdkv_t;
+/** External context type. */
+typedef sdsdkv_t* sdsdkv_context;
+
+enum {
+    /** Success. */
+    SDSDKV_SUCCESS = 0,
+    /** Success, but already done. */
+    SDSDKV_SUCCESS_ALREADY_DONE,
+    /** General error---fatal. */
+    SDSDKV_ERR,
+    /** System error---fatal. */
+    SDSDKV_ERR_SYS,
+    /** Out of resources error---fatal. */
+    SDSDKV_ERR_OOR,
+    /** Invalid argument provided to library---usually fatal. */
+    SDSDKV_ERR_INVLD_ARG,
+    /** Library call before sdsdkv_init was called---improper use of library. */
+    SDSDKV_ERR_CALL_BEFORE_INIT,
+    /** MPI error---fatal. */
+    SDSDKV_ERR_MPI,
+    /** Action not supported---usually not fatal. */
+    SDSDKV_ERR_NOT_SUPPORTED,
+    /** The thing that you were looking for wasn't found---not fatal. */
+    SDSDKV_ERR_NOT_FOUND
+};
 
 #ifdef __cplusplus
 }
